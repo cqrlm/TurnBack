fd6283d2b42c823d88a12a9335bbf37fddaaaf41
cqrlm <al.yak1407@gmail.com>
2024-03-26 17:15:05 +0300

    Implement bottom navigation

--- app/src/main/java/com/example/turnback/MainActivity.kt
+++ app/src/main/java/com/example/turnback/MainActivity.kt
@@ -4,35 +4,34 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LifecycleEventEffect
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.turnback.navigaiton.Screen
+import com.example.turnback.ui.stopwatch.StopwatchScreen
 import com.example.turnback.ui.theme.TurnBackTheme
-import com.example.turnback.ui.theme.Typography
-import com.example.turnback.utils.formatTime
+import com.example.turnback.ui.timer.TimerScreen
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.time.Duration
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -49,63 +48,72 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MainScreen(viewModel: MainViewModel = viewModel()) {
-    val time = viewModel.timeFlow.collectAsState()
+private fun MainScreen() {
+    val navController = rememberNavController()
 
-    LifecycleEventEffect(Lifecycle.Event.ON_PAUSE) {
-        viewModel.saveTime(time.value)
-    }
-
-    MainContent(time.value) {
-        viewModel.resetTime()
-    }
-}
-
-@Composable
-fun MainContent(time: Duration, resetTime: () -> Unit) {
-    Scaffold { paddingValues ->
-        Box(
-            contentAlignment = Alignment.Center,
+    Scaffold(
+        bottomBar = { BottomNavBar(navController) }
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.BottomBarItem.Timer.route,
+            enterTransition = { EnterTransition.None },
+            exitTransition = { ExitTransition.None },
             modifier = Modifier
-                .fillMaxSize()
                 .padding(paddingValues)
+                .fillMaxSize()
         ) {
-            Icon(
-                imageVector = Icons.Filled.DateRange,
-                contentDescription = "icon",
-                tint = MaterialTheme.colorScheme.primary,
-                modifier = Modifier
-                    .padding(top = 40.dp)
-                    .align(Alignment.TopCenter)
-            )
-
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(
-                    text = time.formatTime(),
-                    style = Typography.displayLarge
-                )
-
-                Spacer(modifier = Modifier.height(20.dp))
-
-                Button(
-                    contentPadding = PaddingValues(20.dp),
-                    onClick = resetTime,
-                    modifier = Modifier
-                        .padding(horizontal = 20.dp)
-                        .fillMaxWidth()
-                ) {
-                    Text(text = "Turn back")
-                }
-            }
+            composable(Screen.BottomBarItem.Timer.route) { TimerScreen() }
+            composable(Screen.BottomBarItem.Stopwatch.route) { StopwatchScreen() }
         }
     }
 }
 
-@Preview(apiLevel = 33)
-@Preview(apiLevel = 33, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun BottomNavBar(navController: NavHostController) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    val items = remember {
+        listOf(Screen.BottomBarItem.Timer, Screen.BottomBarItem.Stopwatch)
+    }
+
+    NavigationBar {
+        items.forEach { screen ->
+            NavigationBarItem(
+                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                },
+                label = { Text(text = stringResource(id = screen.titleId)) },
+                icon = {
+                    Icon(
+                        painter = painterResource(screen.iconId),
+                        contentDescription = screen.route
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 private fun MainScreenPreview() {
     TurnBackTheme {
-        MainContent(Duration.ZERO) {}
+        MainScreen()
     }
 }
