78ca12d7885c3150a1508075576c7d39bad30b35
cqrlm <al.yak1407@gmail.com>
2024-03-27 15:33:54 +0300

    Use top bar and remove private bottom bar

--- app/src/main/java/com/example/turnback/MainActivity.kt
+++ app/src/main/java/com/example/turnback/MainActivity.kt
@@ -8,26 +8,20 @@ import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavDestination.Companion.hierarchy
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.turnback.navigaiton.Screen
+import com.example.turnback.ui.bars.AppBar
+import com.example.turnback.ui.bars.BottomNavBar
 import com.example.turnback.ui.stopwatch.StopwatchScreen
 import com.example.turnback.ui.theme.TurnBackTheme
 import com.example.turnback.ui.timer.TimerScreen
@@ -50,9 +44,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun MainScreen() {
     val navController = rememberNavController()
+    var currentScreen: Screen by remember {
+        mutableStateOf(Screen.BottomBarItem.Timer)
+    }
 
     Scaffold(
-        bottomBar = { BottomNavBar(navController) }
+        topBar = { AppBar(currentScreen) },
+        bottomBar = { BottomNavBar(navController) { screen -> currentScreen = screen } }
     ) { paddingValues ->
         NavHost(
             navController = navController,
@@ -65,46 +63,6 @@ private fun MainScreen() {
         ) {
             composable(Screen.BottomBarItem.Timer.route) { TimerScreen() }
             composable(Screen.BottomBarItem.Stopwatch.route) { StopwatchScreen() }
-        }
-    }
-}
-
-@Composable
-private fun BottomNavBar(navController: NavHostController) {
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentDestination = navBackStackEntry?.destination
-
-    val items = remember {
-        listOf(Screen.BottomBarItem.Timer, Screen.BottomBarItem.Stopwatch)
-    }
-
-    NavigationBar {
-        items.forEach { screen ->
-            NavigationBarItem(
-                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-                onClick = {
-                    navController.navigate(screen.route) {
-                        // Pop up to the start destination of the graph to
-                        // avoid building up a large stack of destinations
-                        // on the back stack as users select items
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
-                        }
-                        // Avoid multiple copies of the same destination when
-                        // reselecting the same item
-                        launchSingleTop = true
-                        // Restore state when reselecting a previously selected item
-                        restoreState = true
-                    }
-                },
-                label = { Text(text = stringResource(id = screen.titleId)) },
-                icon = {
-                    Icon(
-                        painter = painterResource(screen.iconId),
-                        contentDescription = screen.route
-                    )
-                }
-            )
         }
     }
 }
