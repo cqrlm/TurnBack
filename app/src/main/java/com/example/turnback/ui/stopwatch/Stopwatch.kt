4c9f0469b5595923d0e266c13eac4cd5c6f27088
cqrlm <al.yak1407@gmail.com>
2024-03-28 15:48:04 +0300

    Replace button with segmented buttons in stopwatch screen

--- app/src/main/java/com/example/turnback/ui/stopwatch/Stopwatch.kt
+++ app/src/main/java/com/example/turnback/ui/stopwatch/Stopwatch.kt
@@ -3,19 +3,29 @@ package com.example.turnback.ui.stopwatch
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -39,12 +49,15 @@ fun StopwatchScreen(viewModel: StopwatchViewModel = hiltViewModel()) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun StopwatchContent(time: Duration, resetTime: () -> Unit) {
     Surface {
         Box(
             contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(30.dp)
         ) {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
@@ -54,14 +67,44 @@ private fun StopwatchContent(time: Duration, resetTime: () -> Unit) {
 
                 Spacer(modifier = Modifier.height(20.dp))
 
-                Button(
-                    contentPadding = PaddingValues(20.dp),
-                    onClick = resetTime,
-                    modifier = Modifier
-                        .padding(horizontal = 20.dp)
-                        .fillMaxWidth()
-                ) {
-                    Text(text = "Turn back")
+                SingleChoiceSegmentedButtonRow(modifier = Modifier.fillMaxWidth()) {
+                    var selectedIndex by remember { mutableIntStateOf(0) }
+
+                    val buttonsData = remember {
+                        StopwatchButtonData.getAll(
+                            playAction = {},
+                            pauseAction = {},
+                            stopAction = { resetTime() }
+                        )
+                    }
+
+                    buttonsData.forEachIndexed { index, buttonData ->
+                        with(buttonData) {
+                            val selected by remember(selectedIndex) {
+                                mutableStateOf(index == selectedIndex)
+                            }
+
+                            SegmentedButton(
+                                selected = selected,
+                                shape = SegmentedButtonDefaults.itemShape(
+                                    index = index,
+                                    count = buttonsData.size
+                                ),
+                                icon = {},
+                                onClick = {
+                                    if (!selected) {
+                                        selectedIndex = index
+                                        onClick()
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    painter = painterResource(iconId),
+                                    contentDescription = stringResource(descriptionId)
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
