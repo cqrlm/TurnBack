d32b1f653c0836dc408acd9084f407251eebe906
cqrlm <al.yak1407@gmail.com>
2024-04-19 16:47:01 +0300

    Define database module for DI

---
+++ app/src/main/java/com/example/turnback/di/DatabaseModule.kt
@@ -0,0 +1,28 @@
+package com.example.turnback.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.turnback.database.AppDatabase
+import com.example.turnback.database.dao.TimerPresetDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext appContext: Context): AppDatabase =
+        Room
+            .databaseBuilder(appContext, AppDatabase::class.java, "app_database")
+            .build()
+
+    @Provides
+    fun provideTimerPresetDao(appDatabase: AppDatabase): TimerPresetDao =
+        appDatabase.timerPresetDao()
+}
