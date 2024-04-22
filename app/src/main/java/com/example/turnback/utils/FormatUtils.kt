74669d56d3cc95bc992dc25f123f882c366e0b64
cqrlm <al.yak1407@gmail.com>
2024-04-22 12:32:29 +0300

    Add function to format elapsed time

--- app/src/main/java/com/example/turnback/utils/FormatUtils.kt
+++ app/src/main/java/com/example/turnback/utils/FormatUtils.kt
@@ -1,10 +1,14 @@
 package com.example.turnback.utils
 
+import android.text.format.DateUtils
 import kotlin.time.Duration
 
 fun Duration.formatTime(): String =
     toComponents { hours, minutes, seconds, _ ->
         TIME_PATTERN.format(hours, minutes, seconds)
     }
+
+fun Duration.formatElapsedTime(): String =
+    DateUtils.formatElapsedTime(inWholeSeconds)
 
 private const val TIME_PATTERN = "%02d:%02d:%02d"
