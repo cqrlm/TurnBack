5a36bd530305bb28574add905251520fbd406757
cqrlm <al.yak1407@gmail.com>
2024-04-25 14:01:56 +0300

    Add DB-related functions to timer view model

--- app/src/main/java/com/example/turnback/ui/timer/TimerViewModel.kt
+++ app/src/main/java/com/example/turnback/ui/timer/TimerViewModel.kt
@@ -2,27 +2,40 @@ package com.example.turnback.ui.timer
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.turnback.database.entities.TimerPresetDBO
+import com.example.turnback.database.repositories.TimerPresetRepository
+import com.example.turnback.mappers.toTimerPreset
+import com.example.turnback.mappers.toTimerPresetDBO
+import com.example.turnback.model.TimerPreset
 import com.example.turnback.services.timer.TimerService
 import com.example.turnback.ui.timer.state.TimerScreenState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.time.Duration
 
 @HiltViewModel
 class TimerViewModel @Inject constructor(
-    private val timerService: TimerService
+    private val timerService: TimerService,
+    private val timerPresetRepository: TimerPresetRepository
 ) : ViewModel() {
 
     val screenState = combine(
         timerService.timeFlow,
-        timerService.timerState
-    ) { time, timerState ->
+        timerService.timerState,
+        timerPresetRepository.getAll().flowOn(Dispatchers.IO)
+    ) { time, timerState, timerPresets ->
         TimerScreenState(
             timerState = timerState,
-            timerDuration = time
+            timerDuration = time,
+            times = timerPresets
+                .map(TimerPresetDBO::toTimerPreset)
+                .sortedBy(TimerPreset::order)
         )
     }.stateIn(viewModelScope, SharingStarted.Eagerly, TimerScreenState())
 
@@ -40,5 +53,18 @@ class TimerViewModel @Inject constructor(
 
     fun stop() {
         timerService.stop()
+    }
+
+    fun save(timerPreset: TimerPreset) {
+        viewModelScope.launch(Dispatchers.IO) {
+            timerPresetRepository.insert(timerPreset.toTimerPresetDBO())
+        }
+    }
+
+    fun delete(timerPresets: List<TimerPreset>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val timerPresetsDBOS = timerPresets.map(TimerPreset::toTimerPresetDBO).toTypedArray()
+            timerPresetRepository.delete(*timerPresetsDBOS)
+        }
     }
 }
