574b65a9c209f05af95e99a69e5dda8dc2bcc45b
cqrlm <al.yak1407@gmail.com>
2024-04-25 14:02:38 +0300

    Add save and delete timer screen actions

--- app/src/main/java/com/example/turnback/ui/timer/state/TimerScreenActions.kt
+++ app/src/main/java/com/example/turnback/ui/timer/state/TimerScreenActions.kt
@@ -1,10 +1,13 @@
 package com.example.turnback.ui.timer.state
 
+import com.example.turnback.model.TimerPreset
 import kotlin.time.Duration
 
 data class TimerScreenActions(
     val start: (Duration) -> Unit = {},
     val pause: () -> Unit = {},
     val resume: () -> Unit = {},
-    val stop: () -> Unit = {}
+    val stop: () -> Unit = {},
+    val save: (TimerPreset) -> Unit = {},
+    val delete: (List<TimerPreset>) -> Unit = {}
 )
