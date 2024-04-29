b4751d308e56531b325f2a221892aeb84393ae78
cqrlm <al.yak1407@gmail.com>
2024-04-29 16:43:38 +0300

    Add common single choice column and dialog

---
+++ app/src/main/java/com/example/turnback/ui/common/SingleChoiceColumn.kt
@@ -0,0 +1,56 @@
+package com.example.turnback.ui.common
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.turnback.ui.theme.TurnBackTheme
+
+@Composable
+fun <T> SingleChoiceColumn(
+    options: List<Pair<T, String>>,
+    selected: T,
+    select: (T) -> Unit
+) {
+    Column {
+        options.forEach { (option, text) ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { select(option) }
+            ) {
+                RadioButton(
+                    selected = option == selected,
+                    onClick = { select(option) }
+                )
+
+                Text(text = text)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SingleChoiceColumnPreview() {
+    val options = listOf("First", "Second", "Third")
+    var selected by remember { mutableIntStateOf(0) }
+
+    TurnBackTheme {
+        SingleChoiceColumn(
+            options = options.mapIndexed { index, option -> index to option },
+            selected = selected
+        ) { selection -> selected = selection }
+    }
+}
---
+++ app/src/main/java/com/example/turnback/ui/common/SingleChoiceDialog.kt
@@ -0,0 +1,60 @@
+package com.example.turnback.ui.common
+
+import android.content.res.Configuration
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.turnback.R
+import com.example.turnback.ui.theme.TurnBackTheme
+
+@Composable
+fun <T> SingleChoiceDialog(
+    title: String,
+    dismiss: () -> Unit,
+    options: List<Pair<T, String>>,
+    selected: T,
+    select: (T) -> Unit
+) {
+    AlertDialog(
+        title = { Text(text = title) },
+        onDismissRequest = dismiss,
+        dismissButton = {
+            TextButton(onClick = dismiss) {
+                Text(text = stringResource(id = R.string.cancel))
+            }
+        },
+        confirmButton = {},
+        text = {
+            SingleChoiceColumn(
+                options = options,
+                selected = selected,
+                select = select
+            )
+        }
+    )
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun SingleChoiceDialogPreview() {
+    val options = listOf("First", "Second", "Third")
+    var selected by remember { mutableIntStateOf(0) }
+
+    TurnBackTheme {
+        SingleChoiceDialog(
+            title = "Title",
+            dismiss = {},
+            options = options.mapIndexed { index, option -> index to option },
+            selected = selected,
+            select = { selection -> selected = selection }
+        )
+    }
+}
