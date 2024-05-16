fedb66ba5961b7b9013271150677343bc17c640f
cqrlm <al.yak1407@gmail.com>
2024-05-16 18:44:46 +0300

    Add editing mode flag to timer screen state

--- app/src/main/java/com/example/turnback/ui/timer/state/TimerScreenState.kt
+++ app/src/main/java/com/example/turnback/ui/timer/state/TimerScreenState.kt
@@ -1,6 +1,8 @@
 package com.example.turnback.ui.timer.state
 
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.example.turnback.model.TimerPreset
 import com.example.turnback.services.timer.TimerState
@@ -10,5 +12,6 @@ import kotlin.time.Duration.Companion.seconds
 data class TimerScreenState(
     val timerState: TimerState = TimerState.STOP,
     val timerDuration: Duration = 0.seconds,
-    val timerPresets: SnapshotStateList<TimerPreset> = mutableStateListOf()
+    val timerPresets: SnapshotStateList<TimerPreset> = mutableStateListOf(),
+    val isEditingMode: MutableState<Boolean> = mutableStateOf(false)
 )
