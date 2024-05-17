22bb8c3b39726871125814baf76ad85640832251
cqrlm <al.yak1407@gmail.com>
2024-05-17 22:27:42 +0300

    Update main view model
    - observe theme state as flow
    - create screen state inside view model
    - add functions to change screen and finish editing

--- app/src/main/java/com/example/turnback/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/turnback/ui/main/MainViewModel.kt
@@ -2,11 +2,19 @@ package com.example.turnback.ui.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.turnback.AppState
+import com.example.turnback.navigaiton.Screen
+import com.example.turnback.services.AppStateService
 import com.example.turnback.services.SharedPreferencesService
 import com.example.turnback.services.timer.TimerPresetSelectorService
 import com.example.turnback.services.timer.TimerPresetService
+import com.example.turnback.ui.main.state.MainScreenState
 import com.example.turnback.ui.theme.ThemeState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,12 +22,21 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val timerPresetSelectorService: TimerPresetSelectorService,
     private val timerPresetService: TimerPresetService,
-    private val sharedPreferencesService: SharedPreferencesService
+    private val sharedPreferencesService: SharedPreferencesService,
+    private val appStateService: AppStateService
 ) : ViewModel() {
 
-    val selectedTimerPresetsCount = timerPresetSelectorService.selectedTimerPresetsCountFlow
+    private val themeStateFlow = MutableStateFlow(sharedPreferencesService.getThemeState())
 
-    val themeState = sharedPreferencesService.getThemeState()
+    val screenState = combine(
+        themeStateFlow,
+        appStateService.appStateFlow
+    ) { themeState, appState ->
+        MainScreenState(
+            themeState = themeState,
+            appState = appState
+        )
+    }.stateIn(viewModelScope, SharingStarted.Eagerly, MainScreenState())
 
     fun clearSelection() {
         viewModelScope.launch {
@@ -34,6 +51,30 @@ class MainViewModel @Inject constructor(
     }
 
     fun setThemeState(themeState: ThemeState) {
+        viewModelScope.launch {
+            themeStateFlow.emit(themeState)
+        }
+
         sharedPreferencesService.setThemeState(themeState)
+    }
+
+    fun changeScreen(newScreen: Screen) {
+        val currentAppState = screenState.value.appState
+
+        if (currentAppState.screen != newScreen) {
+            if (currentAppState is AppState.Deletion) {
+                clearSelection()
+            }
+
+            viewModelScope.launch {
+                appStateService.setAppState(AppState.Idle(newScreen))
+            }
+        }
+    }
+
+    fun finishEditing() {
+        viewModelScope.launch {
+            appStateService.setAppState(AppState.Idle())
+        }
     }
 }
