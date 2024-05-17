3a5826d82e53206f7000450c7ab6ba5356aad6e8
cqrlm <al.yak1407@gmail.com>
2024-05-17 22:23:16 +0300

    Update deletion app state in timer preset selector service

--- app/src/main/java/com/example/turnback/services/timer/TimerPresetSelectorService.kt
+++ app/src/main/java/com/example/turnback/services/timer/TimerPresetSelectorService.kt
@@ -3,20 +3,22 @@ package com.example.turnback.services.timer
 import com.example.turnback.database.entities.TimerPresetDBO
 import com.example.turnback.mappers.toTimerPresetDBO
 import com.example.turnback.model.TimerPreset
+import com.example.turnback.services.AppStateService
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @ActivityRetainedScoped
-class TimerPresetSelectorService @Inject constructor() {
+class TimerPresetSelectorService @Inject constructor(private val appStateService: AppStateService) {
 
     private val _selectedTimerPresetsFlow = MutableStateFlow(emptySet<TimerPreset>())
-    val selectedTimerPresetsFlow = _selectedTimerPresetsFlow.asStateFlow()
 
-    val selectedTimerPresetsCountFlow = _selectedTimerPresetsFlow.map { it.size }
+    val selectedTimerPresetsFlow = _selectedTimerPresetsFlow
+        .asStateFlow()
+        .onEach { timerPresets -> appStateService.updateDeletionAppState(timerPresets.size) }
 
     fun select(timerPreset: TimerPreset) {
         _selectedTimerPresetsFlow.update { timerPresets ->
