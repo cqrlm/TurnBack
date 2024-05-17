3b3ceac043a215b7454748102cbe28254566583b
cqrlm <al.yak1407@gmail.com>
2024-05-17 22:25:08 +0300

    Use app state in main screen state

--- app/src/main/java/com/example/turnback/ui/main/state/MainScreenState.kt
+++ app/src/main/java/com/example/turnback/ui/main/state/MainScreenState.kt
@@ -1,13 +1,9 @@
 package com.example.turnback.ui.main.state
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import com.example.turnback.navigaiton.Screen
+import com.example.turnback.AppState
 import com.example.turnback.ui.theme.ThemeState
 
 data class MainScreenState(
-    val currentScreen: Screen = Screen.BottomBarItem.Timer,
     val themeState: ThemeState = ThemeState.SYSTEM,
-    val selectedTimerPresetsCount: Int = 0,
-    val isEditingMode: MutableState<Boolean> = mutableStateOf(false)
+    val appState: AppState = AppState.Idle()
 )
