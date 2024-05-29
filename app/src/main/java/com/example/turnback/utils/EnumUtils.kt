5ca136b7f6e8953651968965a88988e63fcf3dee
cqrlm <al.yak1407@gmail.com>
2024-05-29 23:46:48 +0300

    Remove experimental annotation from enum utils

--- app/src/main/java/com/example/turnback/utils/EnumUtils.kt
+++ app/src/main/java/com/example/turnback/utils/EnumUtils.kt
@@ -2,6 +2,5 @@ package com.example.turnback.utils
 
 import kotlin.enums.enumEntries
 
-@OptIn(ExperimentalStdlibApi::class)
 inline fun <reified T : Enum<T>> enumValueOrDefault(index: Int, defaultValue: T): T =
     runCatching { enumEntries<T>()[index] }.getOrDefault(defaultValue)
