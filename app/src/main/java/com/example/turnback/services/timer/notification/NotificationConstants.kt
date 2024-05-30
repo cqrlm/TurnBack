2eb528dae54b0af5492303d8cbccb1ae8de611f4
cqrlm <al.yak1407@gmail.com>
2024-05-30 13:36:30 +0300

    Add object to store timer notification constants

---
+++ app/src/main/java/com/example/turnback/services/timer/notification/NotificationConstants.kt
@@ -0,0 +1,11 @@
+package com.example.turnback.services.timer.notification
+
+object NotificationConstants {
+
+    const val CHANNEL_ID = "timer_notifications"
+    const val NOTIFICATION_ID = 7
+    const val CLICK_REQUEST_CODE = 10
+    const val PAUSE_REQUEST_CODE = 11
+    const val RESUME_REQUEST_CODE = 12
+    const val STOP_REQUEST_CODE = 13
+}
