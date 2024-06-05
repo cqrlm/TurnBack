45f80752b88393c2686987453dcf1bfed243b280
cqrlm <al.yak1407@gmail.com>
2024-06-05 19:19:54 +0300

    Add common fade animated visibility function

---
+++ app/src/main/java/com/example/turnback/ui/common/FadeAnimatedVisibility.kt
@@ -0,0 +1,23 @@
+package com.example.turnback.ui.common
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FadeAnimatedVisibility(
+    visible: Boolean,
+    modifier: Modifier = Modifier,
+    content: @Composable AnimatedVisibilityScope.() -> Unit
+) {
+    AnimatedVisibility(
+        visible = visible,
+        enter = fadeIn(),
+        exit = fadeOut(),
+        modifier = modifier,
+        content = content
+    )
+}
