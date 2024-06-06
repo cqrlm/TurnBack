242b11333069d16828506f015f44431134008682
cqrlm <al.yak1407@gmail.com>
2024-06-06 15:10:46 +0300

    Show minus sign if duration is negative in formatted string

--- app/src/main/java/com/example/turnback/utils/FormatUtils.kt
+++ app/src/main/java/com/example/turnback/utils/FormatUtils.kt
@@ -8,11 +8,18 @@ fun Duration.formatTime(): String =
     }
 
 fun Duration.formatElapsedTime(): String =
-    toComponents { hours, minutes, seconds, _ ->
-        if (hours > 0) {
-            TIME_PATTERN.format(hours, minutes, seconds)
-        } else ELAPSED_TIME_PATTERN.format(minutes, seconds)
+    buildString {
+        if (isNegative()) {
+            append(MINUS_SIGN)
+        }
+
+        absoluteValue.toComponents { hours, minutes, seconds, _ ->
+            if (hours > 0) {
+                append(TIME_PATTERN.format(hours, minutes, seconds))
+            } else append(ELAPSED_TIME_PATTERN.format(minutes, seconds))
+        }
     }
 
 private const val TIME_PATTERN = "%02d:%02d:%02d"
 private const val ELAPSED_TIME_PATTERN = "%02d:%02d"
+private const val MINUS_SIGN = '-'
