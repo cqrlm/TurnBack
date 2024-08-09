d89e1da2a2e017d8c4e03e5d1b2a76653132fb6e
cqrlm <al.yak1407@gmail.com>
2024-08-09 19:06:41 +0300

    Don't use timer service flows in timer view model to separate logic

--- app/src/main/java/com/example/turnback/ui/timer/TimerViewModel.kt
+++ app/src/main/java/com/example/turnback/ui/timer/TimerViewModel.kt
@@ -2,45 +2,29 @@ package com.example.turnback.ui.timer
 
 import androidx.lifecycle.viewModelScope
 import com.example.turnback.model.TimerPreset
-import com.example.turnback.services.timer.TimerState
 import com.example.turnback.services.timer.preset.TimerPresetManager
 import com.example.turnback.ui.base.ScreenViewModel
 import com.example.turnback.ui.timer.state.TimerScreenActions
 import com.example.turnback.ui.timer.state.TimerScreenState
-import dagger.assisted.Assisted
-import dagger.assisted.AssistedFactory
-import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import kotlin.time.Duration
+import javax.inject.Inject
 
-@HiltViewModel(assistedFactory = TimerViewModel.Factory::class)
-class TimerViewModel @AssistedInject constructor(
+@HiltViewModel
+class TimerViewModel @Inject constructor(
     private val timerPresetManager: TimerPresetManager,
-    @Assisted val timeFlow: Flow<Duration>,
-    @Assisted val timerStateFlow: Flow<TimerState>
 ) : ScreenViewModel<TimerScreenState, TimerScreenActions>() {
-
-    @AssistedFactory
-    interface Factory {
-        fun create(timeFlow: Flow<Duration>, timerStateFlow: Flow<TimerState>): TimerViewModel
-    }
 
     override val screenState: StateFlow<TimerScreenState> =
         combine(
-            timeFlow,
-            timerStateFlow,
             timerPresetManager.timerPresetsFlow,
             timerPresetManager.timerEditModeFlow
-        ) { time, timerState, timerPresets, timerEditMode ->
+        ) { timerPresets, timerEditMode ->
             TimerScreenState(
-                timerState = timerState,
-                timerDuration = time,
                 timerPresets = timerPresets,
                 timerEditMode = timerEditMode
             )
