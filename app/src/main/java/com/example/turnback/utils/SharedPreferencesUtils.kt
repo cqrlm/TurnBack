11112f591e9133079fb74dc225eae27c1aadf722
cqrlm <al.yak1407@gmail.com>
2024-08-12 21:43:58 +0300

    Add shared preferences utils

---
+++ app/src/main/java/com/example/turnback/utils/SharedPreferencesUtils.kt
@@ -0,0 +1,37 @@
+package com.example.turnback.utils
+
+import com.example.sharedpreferences.SharedPreferencesConstants
+import com.example.sharedpreferences.SharedPreferencesService
+import com.example.turnback.services.stopwatch.StopwatchState
+import com.example.turnback.ui.theme.ThemeState
+
+inline fun <reified T : Enum<T>> SharedPreferencesService.getState(): T =
+    when (T::class) {
+        StopwatchState::class -> getEnumValue<T>(
+            SharedPreferencesConstants.PREFERENCES_STOPWATCH_STATE_KEY,
+            StopwatchState.STOP as T
+        )
+
+        ThemeState::class -> getEnumValue<T>(
+            SharedPreferencesConstants.PREFERENCES_THEME_KEY,
+            ThemeState.SYSTEM as T
+        )
+
+        else -> error("Unknown state type")
+    }
+
+inline fun <reified T : Enum<T>> SharedPreferencesService.saveState(state: T) {
+    when (T::class) {
+        StopwatchState::class -> saveEnumValue(
+            SharedPreferencesConstants.PREFERENCES_STOPWATCH_STATE_KEY,
+            state
+        )
+
+        ThemeState::class -> saveEnumValue(
+            SharedPreferencesConstants.PREFERENCES_THEME_KEY,
+            state
+        )
+
+        else -> error("Unknown state type")
+    }
+}
