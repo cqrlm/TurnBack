c8568af7aaec2ea0fdc326d6abfde3667b4c25cc
cqrlm <al.yak1407@gmail.com>
2024-08-14 13:57:20 +0300

    Make timer preset mapper functions internal

--- core/data/src/main/java/com/example/data/mappers/TimersPresetMapper.kt
+++ core/data/src/main/java/com/example/data/mappers/TimersPresetMapper.kt
@@ -5,8 +5,8 @@ import com.example.database.entities.TimerPresetDBO
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
-fun TimerPresetDBO.toTimerPreset(): TimerPreset =
+internal fun TimerPresetDBO.toTimerPreset(): TimerPreset =
     TimerPreset(order, duration.toDuration(DurationUnit.MILLISECONDS), id)
 
-fun TimerPreset.toTimerPresetDBO(): TimerPresetDBO =
+internal fun TimerPreset.toTimerPresetDBO(): TimerPresetDBO =
     TimerPresetDBO(id, order, duration.inWholeMilliseconds)
