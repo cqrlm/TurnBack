092f50ad1dfde72ac88eb587bc1ebc18e3a80383
cqrlm <al.yak1407@gmail.com>
2024-08-19 22:19:22 +0300

    Add navigation host controller extensions

---
+++ core/navigation/src/main/java/com/example/navigation/NavHostControllerExt.kt
@@ -0,0 +1,26 @@
+package com.example.navigation
+
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+
+fun NavHostController.navigate(screen: Screen) {
+    navigate(screen.route) {
+        // Pop up to the start destination of the graph to
+        // avoid building up a large stack of destinations
+        // on the back stack as users select items
+        popUpTo(graph.findStartDestination().id) {
+            saveState = true
+        }
+        // Avoid multiple copies of the same destination when
+        // reselecting the same item
+        launchSingleTop = true
+        // Restore state when reselecting a previously selected item
+        restoreState = true
+    }
+}
+
+fun NavHostController.isSelected(screen: Screen): Boolean {
+    val currentDestination = currentBackStackEntry?.destination
+    return currentDestination?.hierarchy?.any { it.route == screen.route } == true
+}
