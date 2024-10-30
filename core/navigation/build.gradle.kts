948d298e9dee0ee23cc97af1086c0919e1c4f52a
cqrlm <al.yak1407@gmail.com>
2024-10-31 02:14:11 +0300

    Upgrade compile and target SDK version to 35

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.navigation"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 24
--- core/timer/build.gradle.kts
+++ core/timer/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.timer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 24
