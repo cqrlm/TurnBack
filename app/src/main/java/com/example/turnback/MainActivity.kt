6133b6f00e1522cb8701bdd7ba7f030d6e60bc54
cqrlm <al.yak1407@gmail.com>
2024-11-01 01:04:13 +0300

    Enable edge-to-edge display

--- app/src/main/java/com/example/turnback/MainActivity.kt
+++ app/src/main/java/com/example/turnback/MainActivity.kt
@@ -11,6 +11,7 @@ import android.os.IBinder
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -42,6 +43,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
 
         setContent {
             MainScreen(
